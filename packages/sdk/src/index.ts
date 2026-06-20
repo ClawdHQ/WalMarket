6191aeb5bc79ae306dd509d4c70c4c4148cd0956
Tomiwa Adeyemi <adeyemitomiwa7@gmail.com>
2026-06-20 01:15:22 +0100

    chore(sdk): add package manifest, build config, and public exports

---
+++ packages/sdk/src/index.ts
@@ -0,0 +1,7 @@
+export * from './types.js';
+export * from './walmarket-client.js';
+export * from './event-indexer.js';
+export * from './memwal-connector.js';
+export * from './seal-access.js';
+export * from './tx-utils.js';
+export * from './agent-client.js';
